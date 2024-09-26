d3ff7324d024bcebbebdb22db9823d7e9a0d33ee
Doharabea <95958963+Doharabea@users.noreply.github.com>
2024-09-26 12:00:13 +0300

    recafco

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+(function ($) {
+  "use strict";
+  /*====Dom is loaded==== */
+  var $loader = document.querySelector('#preloader-background');
+  
+  window.onload = function() {
+    setTimeout(function() {
+      $loader.remove(); 
+    }); 
+  };
+
+  /*=====AOS animation=====*/
+  AOS.init({
+    // Global settings:
+    duration: 900,
+    easing: 'ease-in-cubic',
+    disable: 'mobile',
+  });
+  AOS.refresh();
+ 
+  /*==========Counter=========*/
+  $(window).on('scroll', function() {
+    $('.counting').each(function() {
+      var $this = $(this),
+          countTo = $this.attr('data-count');
+      
+      // Check if the element is in view
+      if ($this.offset().top <= $(window).scrollTop() + $(window).height()) {
+        
+        // Start the counting animation
+        $({ countNum: $this.text() }).animate({
+          countNum: countTo
+        }, {
+          duration: 1500,
+          easing: 'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+        });
+      }
+    });
+  });
+})(jQuery);
